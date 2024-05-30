f5dd75bca0e0ba3b381a550cc234f70ce36b05e0
Aman Ranjan <ranjanaman0711@gmail.com>
2024-05-30 20:44:14 +0530

    feat: onboarding service
    google sign in function added

---
+++ lib/src/onboarding/service/onboarding_service.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// Handles authentication with Google OAuth,
+/// creating a new T.R.E.L.L.O user account
+/// if the user is new.
+///
+/// Else it will sign in the user and return the user data.
+class OnboardingService {
+  final GoogleSignIn googleSignInProvider;
+  final FirebaseAuth auth;
+
+  OnboardingService({
+    required this.googleSignInProvider,
+    required this.auth,
+  });
+
+  Future<User?> authenticateWithGoogle() async {
+    try {
+      final user = await googleSignInProvider.signIn();
+      final details = await user!.authentication;
+      final creds = GoogleAuthProvider.credential(
+        accessToken: details.accessToken,
+        idToken: details.idToken,
+      );
+      final userCreds = await auth.signInWithCredential(creds);
+      return userCreds.user;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
